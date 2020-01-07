0d22bc3f51ff476f58c1f2b8e69a9ce79700e922
quoc95 <quoc95pro@gmail.com>
2020-01-07 18:10:25 +0700

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const dbs = require('../utils/dbs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', async (req, res, next) => {
+  let rs = await dbs.execute('select * from city');
+  res.send(rs);
 });
 
 module.exports = router;
