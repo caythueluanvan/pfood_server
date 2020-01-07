55484fb9daeab0bbc9fc3d83d8f70e7920a38580
quoc95 <quoc95pro@gmail.com>
2020-01-07 17:48:25 +0700

    init

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const dbs = require('../utils/dbs');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
+
+
+router.get('/testdb',async function(req, res) {
+  let rs = await dbs.execute('select * from city');
+  res.send(rs);
+});
+
 
 module.exports = router;
---
+++ utils/dbs.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql');
+const config = require('./config.js');
+
+const Service = {
+  
+    getConn: () => {
+      let conn = null;
+        try {
+           conn = mysql.createConnection({
+                host     : config.host,
+                user     : config.user,
+                password : config.password,
+                database : config.database
+              });
+              conn.connect();
+        }
+        catch (err) {
+         
+        }
+      return conn;
+    },
+    closeConn: conn => {
+      if (conn) {
+        try {
+           conn.end();
+        }
+        catch (err) {
+          console.log(err)
+        }
+      }
+    },
+    execute: (sql) => {
+      return new Promise(async (resolve, reject) => {
+        let conn
+        try 
+        {
+          conn = Service.getConn();
+          
+          conn.query(sql, function (error, results) {
+            if (error) reject(error);
+            resolve(results)
+          });
+        }
+        catch (err) {
+          reject(err)
+        }
+        finally {
+          Service.closeConn(conn)
+        }
+      })
+    }
+  }
+  module.exports = Service
