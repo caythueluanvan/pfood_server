e3996e0bdfc0f1b8b140b9a00ea2301e60a3fcbe
quoc95 <quoc95pro@gmail.com>
2020-02-03 21:16:33 +0700

    init

--- routes/user/userprivate.js
+++ routes/user/userprivate.js
@@ -5,15 +5,34 @@ const config = require('../../utils/config');
 
 
 module.exports = (router) => {
+    // Xác thực
     router.use(async (req, res, next) => {
         
         try {
-            let header = req.headers && req.headers.authorization, matches = header ? /^Bearer (\S+)$/.exec(header) : null, token = matches && matches[1]        
-            if (!token) return res.status(403).send({ msg: 'Xác thực không thành công. Vui lòng đăng nhập lại!' })
+            let header = req.headers && req.headers.authorization, matches = header ? /^Bearer (\S+)$/.exec(header) : null, token = matches && matches[1];     
+            if (!token) return res.status(403).send({ msg: 'Xác thực không thành công. Vui lòng đăng nhập lại!' });
+
             jwt.verify(token, config.secret, (err, decoded) => {
-                if (err) return res.status(403).send({ msg: 'Hết phiên làm việc. Vui lòng đăng nhập lại!' })
+                if (err) return res.status(403).send({ msg: 'Hết phiên làm việc. Vui lòng đăng nhập lại!' });
+                console.log(decoded);
+                
+
                 return next();
             })
+        }
+        catch (err) {
+            next(err)
+        }
+    });
+
+    // Check phân quyền
+    router.use(async (req, res, next) => {
+        console.log(req.next);
+        
+        try {
+            console.log(req.path);
+            console.log(req.method);
+            return next();
         }
         catch (err) {
             next(err)
