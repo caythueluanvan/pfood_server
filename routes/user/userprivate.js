39ed459c0404ad64580d845830ec0807fb4956e3
quoc95 <quoc95pro@gmail.com>
2020-02-04 21:24:17 +0700

    init

--- routes/user/userprivate.js
+++ routes/user/userprivate.js
@@ -12,11 +12,18 @@ module.exports = (router) => {
             let header = req.headers && req.headers.authorization, matches = header ? /^Bearer (\S+)$/.exec(header) : null, token = matches && matches[1];     
             if (!token) return res.status(403).send({ msg: 'Xác thực không thành công. Vui lòng đăng nhập lại!' });
 
-            jwt.verify(token, config.secret, (err, decoded) => {
+            jwt.verify(token, config.secret,async (err, decoded) => {
                 if (err) return res.status(403).send({ msg: 'Hết phiên làm việc. Vui lòng đăng nhập lại!' });
-                console.log(decoded);
+                console.log(req.path);
                 
-
+                let per = await dbs.execute(`SELECT gp.* from group_permission gp, map_user_group mug
+                where gp.group_id=mug.group_id and mug.user_id= ? and gp.path = ? and ?? = 1`, [decoded.CustomerID, req.path =='/'?'/user':`/user${req.path}`, req.method=='DELETE' ? 'del' : req.method]) 
+                console.log(per);
+                               
+                if(!per[0]){
+                    return res.status(403).send({ msg: 'Bạn không có quyền truy cập !' });
+                }
+                
                 return next();
             })
         }
@@ -24,23 +31,9 @@ module.exports = (router) => {
             next(err)
         }
     });
-
-    // Check phân quyền
-    router.use(async (req, res, next) => {
-        console.log(req.next);
-        
-        try {
-            console.log(req.path);
-            console.log(req.method);
-            return next();
-        }
-        catch (err) {
-            next(err)
-        }
-    });
     
     /* Get All User */
-    router.get('/data', async (req, res) => {
+    router.get('/', async (req, res) => {
         let rs = await dbs.execute('select * from customer');
         res.json(rs);
     });
