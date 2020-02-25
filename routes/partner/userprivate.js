2f1b0f0cb68f254ca5375de5393c543bc55490c4
quocvc <quocvc95@gmail.com>
2020-02-25 22:22:45 +0700

    updatepartner

--- routes/partner/userprivate.js
+++ routes/partner/userprivate.js
@@ -13,10 +13,23 @@ module.exports = (router) => {
         res.json(rs);
     });
 
-    router.put('/', (req, res) => {
-        console.log(req.body);
-        
-        // let rs = await dbs.execute('SELECT p.PartnerID, p.PartnerName, p.PartnerAddress, p.PartnerEmail, p.PartnerPhone, p.PartnerDescription, p.PartnerImage, c.CityName FROM partner p, city c WHERE p.CityID = c.CityID and CustomerID = ?', [req.params.id]);
-        res.json({a:'a'});
+    router.put('/',async (req, res) => {
+        let PartnerID = req.body.PartnerID;
+        let body = req.body;
+        delete body.PartnerID;
+        let key = Object.keys(body);
+        let value = Object.values(body);
+        let sql = `update partner set `
+        let bind = []
+        key.forEach((e, i) => {
+            sql = sql + (i<key.length-1 ? `?? = ?, ` : `?? = ? `)
+            bind.push(e);
+            bind.push(value[i]);
+        });
+        sql = sql + `WHERE ?? = ?`
+        bind.push('PartnerID');
+        bind.push(PartnerID);
+        let rs = await dbs.execute(sql, bind);
+        res.json(rs);
     });
 };
