0bd5ec5b572767beeadaaa7e6201b011f73ee0e5
quoc95 <quoc95pro@gmail.com>
2020-03-06 18:03:27 +0700

    itit

--- routes/partner/index.js
+++ routes/partner/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const dbs = require('../../utils/dbs');
 const { check, validationResult, body } = require('express-validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../../utils/config');
 const privateRoutePartner = require('./partnerprivate');
 /* Add User */
 router.post('/', [
@@ -61,6 +63,32 @@ router.post('/', [
     }
 
 });
+
+// Sign in
+
+router.post('/signin', async function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+  
+    try {
+      let user = await dbs.execute('select * from customer, partner, city where CustomerUsername = ? and customer.CustomerID = partner.CustomerID and partner.cityID = city.cityID',[username]);    
+      if (user[0]) {
+        let rs = bcrypt.compareSync(password, user[0].CustomerPassword);              
+        if (rs) {
+          delete user[0].CustomerPassword;
+          let path = await dbs.execute('SELECT gp.path, gp.post, gp.get, gp.put, gp.del from group_permission gp, map_user_group mug, customer cu where gp.group_id=mug.group_id and mug.user_id= cu.CustomerID and cu.CustomerUsername =  ?',[username]);
+          var token = jwt.sign(JSON.parse(JSON.stringify(user[0])), config.secret, { expiresIn: config.expires });
+          res.json({ success: true, token: token, expires: new Date(Date.now() + config.expires * 1000), user: user[0], path: path });
+        } else {
+          res.status(401).send({ success: false, msg: 'Sai Tên Đăng Nhập Hoặc Mật Khẩu !' });
+        }
+      } else {
+        res.status(401).send({ success: false, msg: 'Sai Tên Đăng Nhập Hoặc Mật Khẩu !' });
+      }
+    } catch (error) {        
+      res.status(401).send({ success: false, msg: 'Sai Tên Đăng Nhập Hoặc Mật Khẩu !' });
+    }
+  });
 
 privateRoutePartner(router);
 
