faa81e44a3e5e5a6bfa17a88e261b5d4fd2a0680
quoc95 <quoc95pro@gmail.com>
2020-03-25 18:05:29 +0700

    init

--- routes/partner/partnerprivate.js
+++ routes/partner/partnerprivate.js
@@ -13,7 +13,7 @@ module.exports = (router) => {
         res.json(rs);
     });
 
-    router.put('/',async (req, res) => {
+    router.put('/', async (req, res) => {
         let PartnerID = req.body.PartnerID;
         let body = req.body;
         delete body.PartnerID;
@@ -22,7 +22,7 @@ module.exports = (router) => {
         let sql = `update partner set `
         let bind = []
         key.forEach((e, i) => {
-            sql = sql + (i<key.length-1 ? `?? = ?, ` : `?? = ? `)
+            sql = sql + (i < key.length - 1 ? `?? = ?, ` : `?? = ? `)
             bind.push(e);
             bind.push(value[i]);
         });
@@ -30,52 +30,56 @@ module.exports = (router) => {
         bind.push('PartnerID');
         bind.push(PartnerID);
         let rs = await dbs.execute(sql, bind);
-        
-        
+
+
         res.json(rs);
     });
 
-    router.post('/product',async (req, res) => {
-        let productId = await dbs.getNextID('items','itemid');
-        let bind = [productId ,req.body.PartnerID,req.body.ItemName,req.body.description,req.body.img, 0]
+    router.post('/product', async (req, res) => {
+        let productId = await dbs.getNextID('items', 'itemid');
+        let bind = [productId, req.body.PartnerID, req.body.ItemName, req.body.description, req.body.img, 0]
         let rs = await dbs.execute(`insert into items values(?,?,?,?,?,?)`, bind);
-        if(rs.affectedRows > 0){
-            let rsAdd = await dbs.execute(`select i.ItemID, i.ItemName, i.description, i.ItemImage, i.StatusID, s.StatusName from items i, status s where i.statusid=s.statusid and i.ItemID = ? `,[productId])            
-             res.json({type: 'success', msg: 'Thêm thành công !', product: rsAdd});
-        }else{
-            res.json({type: 'fail',  msg: 'Thêm không thành công !'});
+        if (rs.affectedRows > 0) {
+            let rsAdd = await dbs.execute(`select i.ItemID, i.ItemName, i.description, i.ItemImage, i.StatusID, s.StatusName from items i, status s where i.statusid=s.statusid and i.ItemID = ? `, [productId])
+            res.json({ type: 'success', msg: 'Thêm thành công !', product: rsAdd });
+        } else {
+            res.json({ type: 'fail', msg: 'Thêm không thành công !' });
         }
-        
+
     });
 
-    router.get('/product/:partnerid',async (req, res) => {
+    router.get('/product/:partnerid', async (req, res) => {
         let rs = await dbs.execute(`select i.ItemID, i.ItemName, i.description, i.ItemImage, i.StatusID, s.StatusName from items i, status s where i.statusid=s.statusid and PartnerID = ?`, [req.params.partnerid]);
         res.json(rs);
     });
 
-    router.delete('/product/:itemid',async (req, res) => {
+    router.delete('/product/:itemid', async (req, res) => {
         let rs = await dbs.execute(`delete from items where ItemID = ?`, [req.params.itemid]);
-        if(rs.affectedRows > 0){      
-             res.json({type: 'success', msg: 'Xóa thành công !', productId: req.params.itemid});
-        }else{
-            res.json({type: 'fail',  msg: 'Xóa không thành công !'});
+        if (rs.affectedRows > 0) {
+            res.json({ type: 'success', msg: 'Xóa thành công !', productId: req.params.itemid });
+        } else {
+            res.json({ type: 'fail', msg: 'Xóa không thành công !' });
         }
     });
 
-    router.put('/product',async (req, res) => {
+    router.put('/product', async (req, res) => {
         let bind = [req.body.ItemName, req.body.description, req.body.ItemImage, req.body.ItemID];
         let rs = await dbs.execute(`update items set ItemName = ?, description = ?, ItemImage = ? where ItemID = ?`, bind);
         res.json(rs);
     });
     //
 
-    router.post('/sourceofitems',async (req, res) => {
-        let bind = [await dbs.getNextID('sourceofitems','SourceOfItemsID'),req.body.ItemID,req.body.Summary,req.body.Price,new Date(req.body.StartTime),new Date(req.body.EndTime),req.body.Description]
-        let rs = await dbs.execute(`insert into sourceofitems(SourceOfItemsID, ItemID, Summary, Price, StartTime, EndTime, Description) values(?,?,?,?,?,?,?)`, bind);                
-        res.json(rs);
+    router.post('/sourceofitems', async (req, res) => {
+        let bind = [await dbs.getNextID('sourceofitems', 'SourceOfItemsID'), req.body.ItemID, req.body.Summary, req.body.Price, new Date(req.body.StartTime), new Date(req.body.EndTime), req.body.Description, req.body.Image]
+        let rs = await dbs.execute(`insert into sourceofitems(SourceOfItemsID, ItemID, Summary, Price, StartTime, EndTime, Description, Image) values(?,?,?,?,?,?,?, ?)`, bind);
+        if (rs.affectedRows > 0) {
+            res.json({ type: 'success', msg: 'Đăng bán thành công !' });
+        } else {
+            res.json({ type: 'fail', msg: 'Đăng bán không thành công !' });
+        }
     });
 
-    router.get('/sourceofitems/:partnerid',async (req, res) => {
+    router.get('/sourceofitems/:partnerid', async (req, res) => {
         let rs = await dbs.execute(`SELECT s.*, i.ItemName, i.ItemImage FROM sourceofitems s, items i WHERE s.ItemID = i.ItemID and i.partnerid = ? and s.EndTime >= now()`, [req.params.partnerid]);
         res.json(rs);
     });
